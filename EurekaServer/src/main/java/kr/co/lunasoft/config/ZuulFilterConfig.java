a65feeb3da2a5f42ec8cd15ab1e30ed38c599ac0
딘 <jhlee@lunasoft.co.kr>
2020-01-03 16:19:08 +0900

    spring cloud eureka server

--- EurekaServer/src/main/java/kr/co/lunasoft/config/ZuulFilterConfig.java
+++ EurekaServer/src/main/java/kr/co/lunasoft/config/ZuulFilterConfig.java
@@ -6,37 +6,28 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Component
-@Slf4j
 public class ZuulFilterConfig extends ZuulFilter {
 
 	@Override
 	public boolean shouldFilter() {
-		log.info("shouldFilter");
 		return true;
 	}
 
 	@Override
 	public Object run() throws ZuulException {
-		log.info("run");
-		
 		RequestContext requestContext = RequestContext.getCurrentContext();
-        requestContext.addZuulRequestHeader("starting", "EurekaServer");
-		
+		requestContext.addZuulRequestHeader("starting", "EurekaServer");
 		return null;
 	}
 
 	@Override
 	public String filterType() {
-		log.info("filterType");
 		return "pre";
 	}
 
 	@Override
 	public int filterOrder() {
-		log.info("filterOrder");
 		return 0;
 	}
 
